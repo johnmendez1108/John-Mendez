bba9510f7aee478beedf284f84bca0f38171ff28
John Mendez <johnmendez1108@gmail.com>
2015-04-23 08:13:45 +0000

    Changes made by John Mendez (johnmendez1108@gmail.com).

--- TeamStorm App/functions/connection.js
+++ TeamStorm App/functions/connection.js
@@ -4,14 +4,14 @@
     var networkState = navigator.connection.type;
 
     var states = {};
-    states[Connection.UNKNOWN]  = 0;
-	 states[Connection.NONE]     = 1;
-	states[Connection.CELL]     = 2;
-    states[Connection.ETHERNET] = 3;
-    states[Connection.WIFI]     = 4;
-    states[Connection.CELL_2G]  = 5;
-    states[Connection.CELL_3G]  = 6;
-    states[Connection.CELL_4G]  = 7;
+    states[Connection.UNKNOWN]  = 1;
+	states[Connection.NONE]     = 2;
+	states[Connection.CELL]     = 3;
+    states[Connection.ETHERNET] = 4;
+    states[Connection.WIFI]     = 5;
+    states[Connection.CELL_2G]  = 6;
+    states[Connection.CELL_3G]  = 7;
+    states[Connection.CELL_4G]  = 8;
    
    
 
